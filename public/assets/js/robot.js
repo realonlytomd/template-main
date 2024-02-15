5e5fa610a53ca81aed908b7eeb71fb07f4807ae6
realonlytomd <mclaughlintomd@gmail.com>
2024-02-14 19:41:32 -0600

    got currentrobotid

--- public/assets/js/robot.js
+++ public/assets/js/robot.js
@@ -2,6 +2,35 @@ var currentRobotId;
 
 jQuery.noConflict();
 jQuery(document).ready(function( $ ){
+    // initialize vari;ables
+    var currentRobotId = "";
+    // this function happens when Mark clicks the submit a new robot button
+    // 
+    //
+    $(document).on("click", "#submitNewRobot", function(event) {
+        event.preventDefault();
+        console.log("name: ", $("#robotNameInput").val().trim());
+        console.log("bio: ", $("#robotBioInput").val().trim());
+        $.ajax({
+            method: "GET",
+            url: "/createRobot/",
+            data: {
+                name: $("#robotNameInput").val().trim(),
+                bio: $("#robotBioInput").val().trim()
+            }
+        })
+        .then(function(dataRobot) {
+            console.log("Creation of new robot (dataRobot) in robot.js: ", dataRobot);
+            // save id of current (last created) robot
+            console.log("currentRobotId: " + dataRobot._id);
+            currentRobotId = dataRobot._id;
+            // empty out the input fields
+            $("#robotNameInput").val("");
+            $("#robotBioInput").val("");
+            // Hide the current modal, then bring up 2nd modal that allows user to enter kitten metrics.
+            $("#newRobotModal").modal("hide");
+        });
+    });
 
     // this function is after Mark clicks the add image button. A modal appears for him to
     // enter the title, description and browse for an image,
@@ -18,33 +47,7 @@ jQuery(document).ready(function( $ ){
         console.log("in robot.js, dataAddImage, after Robot is populated: ", dataAddImage);
         });
     });
-    // this function happens when Mark clicks the submit a new robot button
-    // 
-    //
-    $(document).on("click", "#submitNewRobot", function(event) {
-        event.preventDefault();
-        console.log("name: ", $("#robotNameInput").val().trim());
-        console.log("bio: ", $("#robotBioInput").val().trim());
-        $.ajax({
-              method: "GET",
-              url: "/createRobot/",
-              data: {
-                name: $("#robotNameInput").val().trim(),
-                bio: $("#robotBioInput").val().trim()
-              }
-          })
-          .then(function(dataRobot) {
-              console.log("Creation of new robot (dataRobot) in robot.js: ", dataRobot);
-              // save id of current (last created) robot
-              // currentRobotId = dataRobot.kitten[dataKittenUser.kitten.length - 1];???
-              console.log("dataRobotId: " + dataRobot._id);
-              // empty out the input fields
-              $("#robotNameInput").val("");
-              $("#robotBioInput").val("");
-              // Hide the current modal, then bring up 2nd modal that allows user to enter kitten metrics.
-              $("#newRobotModal").modal("hide");
-            });
-      });
+  
 
 
 });
